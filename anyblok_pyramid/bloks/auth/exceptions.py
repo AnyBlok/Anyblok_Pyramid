5c5e743d88a1b20b8d9834ae8f92f909776f2048
jssuzanne <jssuzanne@anybox.fr>
2018-02-02 16:12:59 +0100

    Fix RecursionError for python version < 3.5

--- anyblok_pyramid/bloks/auth/exceptions.py
+++ anyblok_pyramid/bloks/auth/exceptions.py
@@ -5,7 +5,13 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
+from anyblok.common import python_version
+
+if python_version() >= (3, 5):
+    MainException = RecursionError
+else:
+    MainException = RuntimeError
 
 
-class RecursionRoleError(RecursionError):
+class RecursionRoleError(MainException):
     """Simple exception to check if the roles is cyclic"""
