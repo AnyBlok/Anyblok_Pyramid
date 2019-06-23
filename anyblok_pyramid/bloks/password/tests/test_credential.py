8e3717fa4846f0286a2cdf8e73fb8c788a83ad18
jssuzanne <js.suzanne@gmail.com>
2019-06-23 19:51:01 +0200

    Refactored test_credential

--- anyblok_pyramid/bloks/password/tests/test_credential.py
+++ anyblok_pyramid/bloks/password/tests/test_credential.py
@@ -5,25 +5,24 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-from anyblok_pyramid.tests.testcase import PyramidBlokTestCase
+import pytest
 from pyramid.httpexceptions import HTTPUnauthorized
 
 
-class TestCredential(PyramidBlokTestCase):
+@pytest.mark.usefixtures('rollback_registry')
+class TestCredential:
 
-    def setUp(self):
-        super(TestCredential, self).setUp()
-        self.User = self.registry.User
-        self.CredentialStore = self.registry.User.CredentialStore
+    def init_user(self, registry):
+        self.User = registry.User
+        self.CredentialStore = registry.User.CredentialStore
         self.User.insert(login='user.1', first_name="User", last_name="1")
         self.CredentialStore.insert(login='user.1', password="P1")
 
-    def test_check_login_ok(self):
-        self.assertEqual(
-            self.User.check_login(login="user.1", password="P1"),
-            'user.1'
-        )
+    def test_check_login_ok(self, rollback_registry):
+        self.init_user(rollback_registry)
+        assert self.User.check_login(login="user.1", password="P1") == 'user.1'
 
-    def test_check_login_ko(self):
-        with self.assertRaises(HTTPUnauthorized):
+    def test_check_login_ko(self, rollback_registry):
+        self.init_user(rollback_registry)
+        with pytest.raises(HTTPUnauthorized):
             self.User.check_login(login="user.1", password="P2")
