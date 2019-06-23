6bba1014fc004eaef506f6bada61a694b7ffc6b9
jssuzanne <js.suzanne@gmail.com>
2019-06-23 19:50:44 +0200

    Refactored test_user

--- anyblok_pyramid/bloks/auth/tests/test_user.py
+++ anyblok_pyramid/bloks/auth/tests/test_user.py
@@ -5,30 +5,29 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-from anyblok.tests.testcase import BlokTestCase
+import pytest
 from ..exceptions import RecursionRoleError
 
 
-class TestUserAndRole(BlokTestCase):
+@pytest.mark.usefixtures('rollback_registry')
+class TestUserAndRole:
 
-    def setUp(self):
-        super(TestUserAndRole, self).setUp()
-        self.User = self.registry.User
-        self.Role = self.registry.User.Role
+    def init_user(self, registry):
+        self.User = registry.User
+        self.Role = registry.User.Role
         self.user = self.User.insert(
             login='user.1', first_name="User", last_name="1")
         self.role = self.Role.insert(name='admin', label="Administrator")
         self.role.users.append(self.user)
 
-    def test_role_name_in_user_roles(self):
-        self.assertIn(
-            self.role.name,
-            self.User.get_roles(self.user.login)
-        )
+    def test_role_name_in_user_roles(self, rollback_registry):
+        self.init_user(rollback_registry)
+        assert self.role.name in self.User.get_roles(self.user.login)
 
-    def test_rec_roles(self):
+    def test_rec_roles(self, rollback_registry):
+        self.init_user(rollback_registry)
         role2 = self.Role.insert(name='g2', label="G2")
         self.role.children.append(role2)
         role2.children.append(self.role)
-        with self.assertRaises(RecursionRoleError):
-            self.registry.flush()
+        with pytest.raises(RecursionRoleError):
+            rollback_registry.flush()
