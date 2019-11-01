2736111889fa202b8cacc1e5172dc4dcb63b24ae
alexis <alexis.tourneux@zeprofile.com>
2019-11-01 13:13:10 +0100

    Added copyrights

--- anyblok_pyramid/bloks/user_identity/__init__.py
+++ anyblok_pyramid/bloks/user_identity/__init__.py
@@ -1,6 +1,7 @@
 # This file is a part of the AnyBlok / Pyramid project
 #
-#    Copyright (C) 2018 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+#    Copyright (C) 2019 Jean-Sebastien SUZANNE <js.suzanne@gmail.com>
+#    Copyright (C) 2019 Alexis TOURNEUX <tourneuxalexis@gmail.com>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
@@ -10,7 +11,7 @@ from anyblok.blok import Blok
 
 class UserIdentity(Blok):
     version = "0.1.0"
-    author = "Jean-Sébastien Suzanne"
+    author = "Jean-Sébastien Suzanne, Alexis Tourneux"
     required = ['anyblok-core', 'auth']
 
     @classmethod
--- anyblok_pyramid/bloks/user_identity/tests/test_user_identity.py
+++ anyblok_pyramid/bloks/user_identity/tests/test_user_identity.py
@@ -1,6 +1,6 @@
 # This file is a part of the AnyBlok / Pyramid project
 #
-#    Copyright (C) 2018 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+#    Copyright (C) 2019 Alexis TOURNEUX <tourneuxalexis@gmail.com>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
