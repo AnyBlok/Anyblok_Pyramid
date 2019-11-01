c75ff74faec11a469969b5710d8e6aa01297e5b1
alexis <alexis.tourneux@zeprofile.com>
2019-11-01 12:20:27 +0100

    Flake8

--- anyblok_pyramid/bloks/user_identity/tests/test_user_identity.py
+++ anyblok_pyramid/bloks/user_identity/tests/test_user_identity.py
@@ -16,6 +16,6 @@ class TestUserIdentity:
         registry = registry_identity
 
         user = registry.User.insert(
-                login="johnny_dowey", first_name="John", last_name="Doe")
+            login="johnny_dowey", first_name="John", last_name="Doe")
 
         assert user.name == "John DOE"
