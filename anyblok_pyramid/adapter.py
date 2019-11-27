a53db93a2b1371753430d8f42853e7e27cfcaedb
alexis <alexis.tourneux@zeprofile.com>
2019-11-27 12:02:42 +0100

    Changed code to create a timedelta_adapter_factory that can be parametrized using an enum

--- anyblok_pyramid/adapter.py
+++ anyblok_pyramid/adapter.py
@@ -13,6 +13,8 @@ from base64 import b64encode
 from decimal import Decimal
 from datetime import timedelta
 
+from enum import Enum, auto, unique
+
 
 def datetime_adapter(obj, request):
     """Format the fields.DateTime to return String
@@ -40,65 +42,88 @@ def datetime_adapter(obj, request):
     return obj.isoformat()
 
 
-def timedelta_adapter(obj, request, mode='seconds'):
+@unique
+class TimeDeltaModes(Enum):
 
-    """Format the fields.TimeDelta to return String
+    """This enum is aimed at creating constants for setting the serialization
+       mode for timedelta adapter."""
 
-    ::
-        from pyramid.renderers import JSON
-        from datetime import timedelta
-        json_renderer = JSON()
-        json_renderer.add_adapter(timedelta, timedelta_adapter)
-        config.add_renderer('json', json_renderer)
+    MICROSECONDS = auto()
+    MILLISECONDS = auto()
+    SECONDS = auto()
+    MINUTES = auto()
+    HOURS = auto()
+    DAYS = auto()
+    WEEKS = auto()
 
-    The mode parameter can be used to set the output of the adpater.
-    This parameter can either be :
 
-        * microseconds
-        * milliseconds
-        * seconds (default)
-        * minutes
-        * hours
-        * days
-        * weeks
+def timedelta_adapter_factory(mode=TimeDeltaModes.SECONDS):
+    def timedelta_adapter(obj, request):
 
-    When using this adapter with non default parameter, it might be used the
-    following way :
+        """Format the fields.TimeDelta to return String
 
-    ::
-        from pyramid.renderers import JSON
-        from datetime import timedelta
-        json_renderer = JSON()
-        json_renderer.add_adapter(
-            timedelta, lambda obj, request: timedelta_adapter(
-                obj, request, 'hours'))
-        config.add_renderer('json', json_renderer)
+        ::
+            from pyramid.renderers import JSON
+            from datetime import timedelta
+            from anyblok_pyramid.adapter import timedelta_adapter_factory
+            json_renderer = JSON()
+            json_renderer.add_adapter(timedelta, timedelta_adapter_factory())
+            config.add_renderer('json', json_renderer)
 
-    :param obj: timedelta obj
-    :param mode: str
-    :rtype: str, seconds corresponding to timedelta
-    """
+        The mode parameter can be used to set the output of the adapter.
+        This parameter can either be :
 
-    if mode == 'microseconds':
-        return obj / timedelta(microseconds=1)
-    elif mode == 'milliseconds':
-        return obj / timedelta(milliseconds=1)
-    elif mode == 'seconds':
-        return obj.total_seconds()
-    elif mode == 'minutes':
-        return obj / timedelta(minutes=1)
-    elif mode == 'hours':
-        return obj / timedelta(hours=1)
-    elif mode == 'days':
-        return obj / timedelta(days=1)
-    elif mode == 'weeks':
-        return obj / timedelta(weeks=1)
-    else:
-        raise ValueError(
-            ("Provided mode for timedelta_adapter is not valid. Found '%s'"
-             "." % mode))
+            * microseconds -> TimeDeltaModes.MICROSECONDS
+            * milliseconds -> TimeDeltaModes.MILLISECONDS
+            * seconds (default) -> TimeDeltaModes.SECONDS
+            * minutes -> TimeDeltaModes.MINUTES
+            * hours -> TimeDeltaModes.HOURS
+            * days -> TimeDeltaModes.DAYS
+            * weeks -> TimeDeltaModes.WEEKS
 
-    return obj.total_seconds()
+        When using this adapter with non default parameter, it might be used the
+        following way :
+
+        ::
+            from pyramid.renderers import JSON
+            from datetime import timedelta
+            from anyblok_pyramid.adapter import (
+                timedelta_adapter_factory,
+                TimeDeltaModes
+            )
+
+            json_renderer = JSON()
+            json_renderer.add_adapter(
+                timedelta, timedelta_adapter_factory(TimeDeltaModes.HOURS))
+            config.add_renderer('json', json_renderer)
+
+        :param obj: timedelta obj
+        :param mode: str
+        :rtype: str, seconds corresponding to timedelta
+        """
+
+        import ipdb; ipdb.set_trace()
+
+        if mode == TimeDeltaModes.MICROSECONDS:
+            return obj / timedelta(microseconds=1)
+        elif mode == TimeDeltaModes.MILLISECONDS:
+            return obj / timedelta(milliseconds=1)
+        elif mode == TimeDeltaModes.SECONDS:
+            return obj.total_seconds()
+        elif mode == TimeDeltaModes.MINUTES:
+            return obj / timedelta(minutes=1)
+        elif mode == TimeDeltaModes.HOURS:
+            return obj / timedelta(hours=1)
+        elif mode == TimeDeltaModes.DAYS:
+            return obj / timedelta(days=1)
+        elif mode == TimeDeltaModes.WEEKS:
+            return obj / timedelta(weeks=1)
+        else:
+            raise ValueError(
+                ("Provided mode for timedelta_adapter is not valid. Found '%s'"
+                 "." % mode))
+
+    return timedelta_adapter
 
 
 def date_adapter(obj, request):
