6ee8c765c26cdc7dab21ca8166686ea9c288800e
Alexis Tourneux <33232248+AlexTorx@users.noreply.github.com>
2019-12-03 09:00:05 +0100

    Created adapter for timedelta and added tests related to new adapter (#27)
    * Added copyrights
    
    * Changed computation according to documentation
    
    * Changed code to create a timedelta_adapter_factory that can be parametrized using an enum
    
    * Modified tests to match new structure
    
    * Updated Changelog

--- anyblok_pyramid/adapter.py
+++ anyblok_pyramid/adapter.py
@@ -2,6 +2,7 @@
 #
 #    Copyright (C) 2017 Franck BRET <franckbret@gmail.com>
 #    Copyright (C) 2017 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+#    Copyright (C) 2019 Alexis TOURNEUX <tourneuxalexis@gmail.com>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
@@ -10,6 +11,9 @@ import pytz
 import time
 from base64 import b64encode
 from decimal import Decimal
+from datetime import timedelta
+
+from enum import Enum, unique
 
 
 def datetime_adapter(obj, request):
@@ -36,6 +40,88 @@ def datetime_adapter(obj, request):
             obj = timezone.localize(obj)
 
     return obj.isoformat()
+
+
+@unique
+class TimeDeltaModes(Enum):
+
+    """This enum is aimed at creating constants for setting the serialization
+       mode for timedelta adapter."""
+
+    MICROSECONDS = 1
+    MILLISECONDS = 2
+    SECONDS = 3
+    MINUTES = 4
+    HOURS = 5
+    DAYS = 6
+    WEEKS = 7
+
+
+def timedelta_adapter_factory(mode=TimeDeltaModes.SECONDS):
+    def timedelta_adapter(obj, request):
+
+        """Format the fields.TimeDelta to return String
+
+        ::
+            from pyramid.renderers import JSON
+            from datetime import timedelta
+            from anyblok_pyramid.adapter import timedelta_adapter_factory
+            json_renderer = JSON()
+            json_renderer.add_adapter(timedelta, timedelta_adapter_factory())
+            config.add_renderer('json', json_renderer)
+
+        The mode parameter can be used to set the output of the adapter.
+        This parameter can either be :
+
+            * microseconds -> TimeDeltaModes.MICROSECONDS
+            * milliseconds -> TimeDeltaModes.MILLISECONDS
+            * seconds (default) -> TimeDeltaModes.SECONDS
+            * minutes -> TimeDeltaModes.MINUTES
+            * hours -> TimeDeltaModes.HOURS
+            * days -> TimeDeltaModes.DAYS
+            * weeks -> TimeDeltaModes.WEEKS
+
+        When using this adapter with non default parameter, it might be used the
+        following way :
+
+        ::
+            from pyramid.renderers import JSON
+            from datetime import timedelta
+            from anyblok_pyramid.adapter import (
+                timedelta_adapter_factory,
+                TimeDeltaModes
+            )
+
+            json_renderer = JSON()
+            json_renderer.add_adapter(
+                timedelta, timedelta_adapter_factory(TimeDeltaModes.HOURS))
+            config.add_renderer('json', json_renderer)
+
+        :param obj: timedelta obj
+        :param mode: str
+        :rtype: str, seconds corresponding to timedelta
+        """
+
+        if mode == TimeDeltaModes.MICROSECONDS:
+            return obj / timedelta(microseconds=1)
+        elif mode == TimeDeltaModes.MILLISECONDS:
+            return obj / timedelta(milliseconds=1)
+        elif mode == TimeDeltaModes.SECONDS:
+            return obj.total_seconds()
+        elif mode == TimeDeltaModes.MINUTES:
+            return obj / timedelta(minutes=1)
+        elif mode == TimeDeltaModes.HOURS:
+            return obj / timedelta(hours=1)
+        elif mode == TimeDeltaModes.DAYS:
+            return obj / timedelta(days=1)
+        elif mode == TimeDeltaModes.WEEKS:
+            return obj / timedelta(weeks=1)
+        else:
+            raise ValueError(
+                ("Provided mode for timedelta_adapter is not valid. Found '%s'"
+                 "." % mode))
+
+    return timedelta_adapter
 
 
 def date_adapter(obj, request):
--- anyblok_pyramid/tests/test_adapter.py
+++ anyblok_pyramid/tests/test_adapter.py
@@ -1,11 +1,12 @@
 # This file is a part of the AnyBlok / Pyramid project
 #
 #    Copyright (C) 2015 Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
+#    Copyright (C) 2019 Alexis TOURNEUX <tourneuxalexis@gmail.com>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from pyramid.renderers import JSON
 from uuid import UUID, uuid1
 from os import urandom
@@ -13,11 +14,14 @@ from decimal import Decimal
 from anyblok_pyramid.adapter import (
     datetime_adapter,
     date_adapter,
+    TimeDeltaModes,
+    timedelta_adapter_factory,
     uuid_adapter,
     bytes_adapter,
     decimal_adapter,
 )
 from anyblok_pyramid.testing import init_web_server
+import pytest
 
 
 class TestAdapter:
@@ -38,6 +42,62 @@ class TestAdapter:
         res = webserver.get('/test/', status=200)
         assert res.json_body['datetime'] == datetime_adapter(
             datetime(2017, 10, 1, 1, 1, 1), None)
+
+    def test_registry_get_timedelta(self):
+
+        def get_timedelta(request):
+            return {'timedelta': timedelta(
+                days=1, hours=4, minutes=56,
+                seconds=3710, milliseconds=4000,
+                microseconds=500)}
+
+        def add_route_and_views(config):
+            config.add_route('dbname', '/test/')
+            config.add_view(
+                get_timedelta, route_name='dbname', renderer='json')
+            json_renderer = JSON()
+            json_renderer.add_adapter(
+                timedelta, timedelta_adapter_factory())
+            config.add_renderer('json', json_renderer)
+
+        webserver = init_web_server(add_route_and_views)
+        res = webserver.get('/test/', status=200)
+
+        assert res.json_body['timedelta'] == timedelta(
+            days=1, hours=4, minutes=56, seconds=3710,
+            milliseconds=4000, microseconds=500).total_seconds()
+
+        # This part is aimed at testing that the different modes implemented on
+        # timedelta adapter are behaving as intented
+        delta = timedelta(
+            weeks=1, days=3, hours=15, minutes=3, seconds=45, milliseconds=354,
+            microseconds=768
+        )
+
+        assert round(
+            timedelta_adapter_factory(TimeDeltaModes.SECONDS)(
+                delta, None), 8) == 918225.354768
+        assert round(
+            timedelta_adapter_factory(TimeDeltaModes.MICROSECONDS)(
+                delta, None), 8) == 918225354768
+        assert round(
+            timedelta_adapter_factory(TimeDeltaModes.MILLISECONDS)(
+                delta, None), 8) == 918225354.768
+        assert round(
+            timedelta_adapter_factory(TimeDeltaModes.MINUTES)(
+                delta, None), 8) == 15303.7559128
+        assert round(
+            timedelta_adapter_factory(TimeDeltaModes.HOURS)(
+                delta, None), 8) == 255.06259855
+        assert round(
+            timedelta_adapter_factory(TimeDeltaModes.DAYS)(
+                delta, None), 8) == 10.62760827
+        assert round(
+            timedelta_adapter_factory(TimeDeltaModes.WEEKS)(
+                delta, None), 8) == 1.51822975
+
+        with pytest.raises(ValueError):
+            timedelta_adapter_factory("not a valid mode")(delta, None)
 
     def test_registry_get_date(self):
 
